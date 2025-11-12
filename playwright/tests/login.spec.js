9b84fa8cd03b039ece084a1c86de64d7f9d7e36d
aldiesta <austindiesta@gmail.com>
2025-11-12 14:38:03 -0600

    negative login test

--- playwright/tests/login.spec.js
+++ playwright/tests/login.spec.js
@@ -13,3 +13,17 @@ test('successful login', async ({ page }) => {
     //Assert new page shows products
     await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
 });
+
+test('unsuccessful login', async ({ page }) => {
+    await page.goto('https://www.saucedemo.com/');
+
+    //Input incorrect credentials
+    await page.locator('[data-test="username"]').fill('standard_user');
+    await page.locator('[data-test="password"]').fill('sauce');
+
+    //Click login button
+    await page.locator('[data-test="login-button"]').click();
+
+    await expect(page.locator('[data-test="error"]')).toContainText('Epic sadface');
+
+});
